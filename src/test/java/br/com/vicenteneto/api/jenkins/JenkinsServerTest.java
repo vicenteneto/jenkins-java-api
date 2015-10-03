f26eaca0c8461a0099dd152f38f80129491d24d0
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2015-10-02 22:55:31 -0300

    Creating first test and adding power mock to the pom.xml

---
+++ src/test/java/br/com/vicenteneto/api/jenkins/JenkinsServerTest.java
@@ -0,0 +1,62 @@
+package br.com.vicenteneto.api.jenkins;
+
+import java.net.URI;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.mashape.unirest.http.Headers;
+import com.mashape.unirest.http.HttpResponse;
+
+import br.com.vicenteneto.api.jenkins.client.JenkinsClient;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(JenkinsServer.class)
+public class JenkinsServerTest {
+	
+	@InjectMocks
+	private JenkinsServer JenkinsServer;
+	
+	@Mock
+	private JenkinsClient jenkinsClientMock;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		PowerMockito.whenNew(JenkinsClient.class).withAnyArguments()
+				.thenReturn(jenkinsClientMock);
+	}
+	
+	@Test
+	public void constructorTest() throws Exception {
+		new JenkinsServer(new URI("http://localhost"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getVersionTest() throws Exception {
+		HttpResponse<String> responseMock = Mockito.mock(HttpResponse.class);
+		Headers headersMock = Mockito.mock(Headers.class);
+		String version = "0.0.0";
+		
+		Mockito.when(jenkinsClientMock.get(Mockito.anyString()))
+				.thenReturn(responseMock);
+		Mockito.when(responseMock.getHeaders())
+				.thenReturn(headersMock);
+		Mockito.when(headersMock.getFirst(Mockito.anyString()))
+				.thenReturn(version);
+		
+		String response = JenkinsServer.getVersion();
+		Assert.assertEquals(response, version);
+	}
+}
